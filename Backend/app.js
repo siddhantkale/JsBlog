671aee5b09494733b8fa1e4beb636a6c16b24df0
SiddhantKale100 <122144668+siddhantkale@users.noreply.github.com>
2024-08-22 22:40:12 +0530

    Set up express app

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const PORT = 3000;
+const app = express();
+
+app.get("/",(req,res)=>{
+    return "hello world";
+});
+
+app.listen(PORT,()=>{
+    console.log("Server running");
+});
